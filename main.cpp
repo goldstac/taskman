9cc3b520d91611c44358b461a45c72a495c6dded
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-20 20:07:27 +0400

    add --version

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <string>
 int main(int argc,char ** argv){
 int real_argc = argc - 1;
+std::string version = "V0.0.1";
 std::string arg1 = argv[1];
 std::string arg2 = argv[2];
 if (arg1 == "add"){
     std::cout << arg2;
+}
+else if (arg1 == "--version"){
+    std::cout << version << "\n";
 }
     return 0;
 }
