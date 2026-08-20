8d1c3f3b325842dc5fe7d1e69f66ee43a54fa88b
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-20 20:42:23 +0400

    major fixs but still not good

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
 int main(int argc,char ** argv){
-int real_argc = argc - 1;
 std::string version = "V0.0.1";
 std::string arg1 = argv[1];
-std::string arg2 = argv[2];
-if (real_argc > 1){
+if (argc > 1){
     if (arg1 == "add"){
+        if (argc < 3){
+            std::cout << "usage: taskman add <task>\n";
+        }
+        std::string arg2 = argv[2];
         std::cout << arg2;
     }
     else if (arg1 == "--version"){
         std::cout << version << "\n";
     }
+}
+else{
+    std::cout << "usage: taskman <command>\n";
 }
 }
