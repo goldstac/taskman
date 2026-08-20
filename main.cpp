a2e9f0973525952952a2018c4cf859547dcad498
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-20 18:13:01 +0400

    add basic add argument parsing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 int main(int argc,char ** argv){
 int real_argc = argc - 1;
 std::string arg1 = argv[1];
-std::cout << arg1;
+std::string arg2 = argv[2];
+if (arg1 == "add"){
+    std::cout << arg2;
+}
     return 0;
 }
