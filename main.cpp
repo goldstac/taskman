45a14e129424fcee48ba43eef678598852f3813e
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-20 17:06:39 +0400

    testing and

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 int main(int argc,char ** argv){
-std::cout << argc;
-
+int real_argc = argc - 1;
+std::string arg1 = argv[1];
+std::cout << arg1;
     return 0;
 }
