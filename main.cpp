707270ac6e3b46948de60a0831fd57e777e376e2
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-20 20:45:01 +0400

    sus

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main(int argc,char ** argv){
 std::string version = "V0.0.1";
-std::string arg1 = argv[1];
 if (argc > 1){
+    std::string arg1 = argv[1];
     if (arg1 == "add"){
         if (argc < 3){
             std::cout << "usage: taskman add <task>\n";
